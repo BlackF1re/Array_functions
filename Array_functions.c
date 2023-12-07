fc91105904a7a2a73c32c738aa667bc344adb852
Павел Левицкий <creative-creative@outlook.com>
2023-12-07 23:34:05 +0700

    fix&upd

--- Array_functions.c
+++ Array_functions.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <float.h>
 
 void error()
 {
@@ -82,12 +83,13 @@ int lesserThan(int length, int* array, int a1) //M1
             counter++;
         }
     }
+    return counter;
 }
 
 int main()
 {
     system("chcp 1251");
-    printf("Выполнено Левицким Павлом, группа 423-2\nВариант 11.\n\n");
+    printf("Выполнено Левицким Павлом, группа 423-2\nВариант 11.\n");
     int lengthOfX, lengthOfY, isDigit;
 
     printf("\nВведите количество элементов массива X:\t");
@@ -98,17 +100,17 @@ int main()
     isDigit = scanf_s("%d", &lengthOfY);
     if (lengthOfY < 1 || isDigit != 1)
         error();
-
     srand(time(0));
+
     int* arrayX = calloc(lengthOfX, sizeof(int));
     for (int i = 0; i < lengthOfX; i++)
     {
         arrayX[i] = rand() % 20 - 10;
     }
-    
     printf("\nМассив X:\n");
     print(lengthOfX, arrayX);
     printf("\n");
+
     int* arrayY = calloc(lengthOfY, sizeof(int));
     for (int i = 0; i < lengthOfY; i++)
     {
@@ -121,26 +123,34 @@ int main()
 
     if (a1x < 0)
     {
+        printf("\n\nСумма элементов массива X (A1(X)):\t%d\t", a1x);
         int a2x = sumOfPositives(lengthOfX, arrayX);
+        printf("\nСумма положительных элементов массива X (A2(X)):\t%d\t", a2x);
         int a4x = countOfZeros(lengthOfX, arrayX);
+        printf("\nКол-во нулевых элементов массива X (A4(X)):\t%d\t", a4x);
         int m1x = lesserThan(lengthOfX, arrayX, a1x);
+        printf("\nКол-во элементов массива X меньше суммы элементов массива X (M1(X)):\t%d\t", m1x);
         int a2y = sumOfPositives(lengthOfY, arrayY);
+        printf("\nСумма положительных элементов массива Y (A2(Y)):\t%d\t\n", a2y);
         float z1 = (a2x + 2.8 * pow(10, -3) * a4x) / (0.3 + m1x * a2y);
-        printf("\nz1 = %lf", &z1);
+        //float z1 = (sumOfPositives(lengthOfX, arrayX) + 2.8 * pow(10, -3) * countOfZeros(lengthOfX, arrayX)) / (0.3 + lesserThan(lengthOfX, arrayX, a1x) * sumOfPositives(lengthOfY, arrayY));
+        printf("\nZ1 = %f", z1);
     }
-    else printf("\nНе было выполнено условие: A1(X) < 0");
+    else printf("\n\nZ1: не было выполнено условие: A1(X) < 0\n");
 
     if (a1x >= 0)
     {
         int a1y = sumOfAll(lengthOfY, arrayY);
+        printf("\nСумма элементов массива Y (A1(Y)):\t%d\t", a1y);
         int m1y = lesserThan(lengthOfY, arrayY, a1y);
+        printf("\nКол-во элементов массива Y меньше суммы элементов массива Y (M1(Y)):\t%d\t", m1y);
         int a5x = sumMinMax(lengthOfX, arrayX);
+        printf("\nСумма максимального и минимального элементов массива X (A5(X)):\t%d\t\n", a5x);
         float z2 = 0.000135 * a5x + m1y;
-        printf("\nz2 = %lf", &z2);
-
+        printf("\nZ2 = %f", z2);
     }
-    else printf("\nНе было выполнено условие: A1(X) >= 0");
-
+    else printf("\n\nZ2: не было выполнено условие: A1(X) >= 0\n");
     free(arrayX);
     free(arrayY);
+    return 0;
 }
